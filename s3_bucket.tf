239130e9608a4462c0aa3e37b8415d2ae9041604
Karl Martin <karlmartin895@gmail.com>
2025-02-21 09:49:43 -0500

    Update s3_bucket.tf

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -5,16 +5,29 @@ terraform {
       version = "5.87.0"
     }
   }
-  required_version = ">= 1.4.6"
 }
 
 provider "aws" {
   region  = "us-east-1"
+  profile = "personal"
 }
 
 
 resource "aws_s3_bucket" "testing" {
-  bucket = "env0-kaugm-s3-bucket-250219"
+  bucket = "env0_kaugm-s3-bucket-250219"
+
+  lifecycle_rule {
+    id      = "archive"
+    enabled = true
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
 
   tags = {
     Purpose = "env0"
